73db4ca29d952613235196a993623c642d090962
Nicolas Rios <dnicolas.r98@gmail.com>
2021-05-19 13:05:19 +0200

    Do 'sumAll' exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (...nums) {
+  for (let num of nums) {
+    if (typeof num !== "number" || num < 0) {
+      return "ERROR";
+    }
+  }
 
+  let result = Number();
+  let min = Math.min(...nums);
+  let max = Math.max(...nums);
+
+  for (let i = min; i <= max; i++) {
+    result += i;
+  }
+
+  return result;
 };
 
+sumAll(1, 4);
 module.exports = sumAll;
