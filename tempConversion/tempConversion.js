5378024cc8b4256070693e7bfeb8262a4258e4af
Nicolas Rios <dnicolas.r98@gmail.com>
2021-05-19 21:21:57 +0200

    Do 'tempConversion' exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function (fahrenheit) {
+  const celsius = (((fahrenheit - 32) * 5) / 9).toFixed(1); //Shows only one decimal
+  return Number(celsius);
 };
 
-const ctof = function() {
-
+const ctof = function (celsius) {
+  const fahrenheit = ((celsius * 9) / 5 + 32).toFixed(1);
+  return Number(fahrenheit);
 };
+
+ctof(73.2);
+ftoc(100);
 
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
