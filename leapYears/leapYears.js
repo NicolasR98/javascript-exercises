0bd97bc1966a9e6d44e79e49de46968a245098b0
Nicolas Rios <dnicolas.r98@gmail.com>
2021-05-19 20:57:54 +0200

    Do 'leapYears' exercise

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  let isLeapYear = Boolean();
 
+  if (year > 900) {
+    if (year % 400 === 0) isLeapYear = true;
+    if (year % 4 === 0 && year % 100 !== 0) isLeapYear = true;
+  }
+
+  return (isLeapYear = false);
 };
+
+leapYears(1900);
+leapYears(1600);
 
 module.exports = leapYears;
