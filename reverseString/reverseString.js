4d6ef38b48c2e384b44d7353a6a0bc89c426133a
Nicolas Rios <dnicolas.r98@gmail.com>
2021-05-19 11:40:43 +0200

    Do 'reverseString' exercise

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  const reverseStr = [...str].reverse().join("");
+  return reverseStr;
 };
 
+reverseString("hello");
 module.exports = reverseString;
