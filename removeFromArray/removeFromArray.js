a14daba0d8ab44bc0ca7bd3767356e35b31e869b
Nicolas Rios <dnicolas.r98@gmail.com>
2021-05-19 12:25:20 +0200

    Do 'removeFromArray' exercise

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...targets) {
+  
+  //Create copy of array to avoid mutation
+  let copyArr = [...arr];
 
+  targets.forEach((target) => {
+    if (copyArr.indexOf(target) !== -1) {
+      let index = copyArr.indexOf(target);
+      copyArr.splice(index, 1);
+    }
+  });
+  return copyArr;
 };
 
 module.exports = removeFromArray;
