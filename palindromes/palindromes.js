4533d5727cc8ee2a9e8d4e9ac1baa8c9dec9d08d
Nicolas Rios <dnicolas.r98@gmail.com>
2021-06-02 19:17:17 +0200

    Do 'palindrome' exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = (str) => {
+  const cleanStr = str.toLowerCase().replace(/[^A-Za-z]/g, "");
+  return cleanStr
+    .split("")
+    .reverse("")
+    .join("") === cleanStr;
 };
 
 module.exports = palindromes;
