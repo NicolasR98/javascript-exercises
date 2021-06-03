aa263ce5e499917d8489f03bbf7bd18a45942846
Nicolas Rios <dnicolas.r98@gmail.com>
2021-06-03 23:20:57 +0200

    Do 'fibonacci' exercise

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = (count) => {
+  if (count < 0) return "OOPS";
+  if (count === 1) return 1;
 
+  let a = 0;
+  let b = 1;
+
+  for (let i = 1; i < count; i++) {
+    const temp = b;
+    b = a + b;
+    a = temp;
+  }
+
+  return b;
 };
+
+console.log(fibonacci(4));
 
 module.exports = fibonacci;
