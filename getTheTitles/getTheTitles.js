60f0ce7801096a001bcfeb4519266dfa835bdc66
Nicolas Rios <dnicolas.r98@gmail.com>
2021-06-03 23:26:03 +0200

    Do 'getTheTitles' exercise

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,15 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: "Book",
+    author: "Name",
+  },
+  {
+    title: "Book2",
+    author: "Name2",
+  },
+];
 
-};
+const getTheTitles = (books) => books.map((book) => book.title);
 
+console.log(getTheTitles(books));
 module.exports = getTheTitles;
